e0a2bb425e9448ec06d3cf97a2738bb7aabf7965
Yyyyyyy3480 <ejhibbard@icloud.com>
2026-03-12 15:19:05 -0400

    Create picklist.vue

---
+++ src/picklist.vue
@@ -0,0 +1,7 @@
+import { generatePicklist } from "@/analytics/picklist"
+
+const scoutingData = JSON.parse(localStorage.getItem("matchData"))
+
+const picklist = generatePicklist(scoutingData)
+
+console.log(picklist)
