b3a3691e88717fdb65472294276a2b83f087eee6
Yyyyyyy3480 <ejhibbard@icloud.com>
2026-03-12 15:23:11 -0400

    Create picklist.js

---
+++ public/picklist.js
@@ -0,0 +1,47 @@
+export function generatePicklist(matchData) {
+  const teams = {}
+
+  matchData.forEach(entry => {
+    const team = entry.team
+
+    if (!teams[team]) {
+      teams[team] = {
+        matches: 0,
+        autoFuel: 0,
+        teleopFuel: 0,
+        climbs: 0,
+        defense: 0
+      }
+    }
+
+    teams[team].matches++
+
+    teams[team].autoFuel += entry.autoFuelScored || 0
+    teams[team].teleopFuel += entry.teleopFuelScored || 0
+
+    if (entry.towerClimb !== "None") {
+      teams[team].climbs++
+    }
+
+    if (entry.playedDefense) {
+      teams[team].defense++
+    }
+  })
+
+  const rankings = Object.keys(teams).map(team => {
+    const t = teams[team]
+
+    const score =
+      (t.autoFuel / t.matches) * 3 +
+      (t.teleopFuel / t.matches) * 2 +
+      (t.climbs / t.matches) * 10 +
+      (t.defense / t.matches) * 2
+
+    return {
+      team,
+      score
+    }
+  })
+
+  return rankings.sort((a, b) => b.score - a.score)
+}
