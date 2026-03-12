2949bb6d82fa7e42f81bedaeabee2c458ca5f956
Yyyyyyy3480 <ejhibbard@icloud.com>
2026-03-12 15:38:42 -0400

    Update picklisst.vue

--- public/picklisst.vue
+++ public/picklisst.vue
@@ -5,3 +5,35 @@ const scoutingData = JSON.parse(localStorage.getItem("matchData"))
 const picklist = generatePicklist(scoutingData)
 
 console.log(picklist)
+<script setup>
+import { ref, onMounted } from "vue"
+import { generatePicklist } from "@/analytics/picklist"
+
+const picklist = ref([])
+
+onMounted(() => {
+  const scoutingData = JSON.parse(localStorage.getItem("matchData")) || []
+  picklist.value = generatePicklist(scoutingData)
+})
+</script>
+
+<template>
+  <div>
+    <h1>Alliance Picklist</h1>
+
+    <table>
+      <tr>
+        <th>Rank</th>
+        <th>Team</th>
+        <th>Score</th>
+      </tr>
+
+      <tr v-for="(team,index) in picklist" :key="team.team">
+        <td>{{ index + 1 }}</td>
+        <td>{{ team.team }}</td>
+        <td>{{ team.score.toFixed(1) }}</td>
+      </tr>
+
+    </table>
+  </div>
+</template>
